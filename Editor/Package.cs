94f61ad0681881c81c86d57b955fda3204f8decf
hananoki <tonkatu@helen.ocn.ne.jp>
2021-06-21 21:15:43 +0900

    ## [2.1.1] - 2021-06-21 - SharedModule v1.10.0 or later

--- Editor/Package.cs
+++ Editor/Package.cs
@@ -4,10 +4,15 @@ using UnityEditor;
 
 namespace HananokiEditor.SymbolSettings {
   public static class Package {
+    public const string reverseDomainName = "com.hananoki.symbol-settings";
     public const string name = "SymbolSettings";
     public const string nameNicify = "Symbol Settings";
     public const string editorPrefName = "Hananoki.SymbolSettings";
-    public const string version = "2.1.1";
+    public const string version = "2.1.2";
+		[HananokiEditorMDViewerRegister]
+		public static string MDViewerRegister() {
+			return "6c5fbe4e4af120342bbc840887dfd73d";
+		}
     public static string projectSettingsPath => $"{SharedModule.SettingsEditor.projectSettingDirectory}/SymbolSettings.json";
   }
 }
